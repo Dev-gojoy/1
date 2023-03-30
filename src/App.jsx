3ec55f8219c43d0625489ea852276aa15882d2cd
Dev-gojoy <32jongwoo@gmail.com>
2023-03-30 15:50:18 +0900

    header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import AppleLogo from "./images/apple.png";
+// import AppleLogo from "./images/apple.png";
 
 function App() {
   return (
@@ -19,6 +19,12 @@ function App() {
           </ul>
         </div>
       </header>
+      <main>
+        {/* Introduce */}
+        <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+          여기에 나를 소개하는 내용을 적으면 됩니다.
+        </div>
+      </main>
     </div>
   );
 }
