05b4a68ee0bbae0efb3880fab4b9a1f27bd91e83
Dev-gojoy <32jongwoo@gmail.com>
2023-03-30 16:50:24 +0900

    myWeb

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@
 
 function App() {
   return (
-    <div className="bg-red-100 min h-screen">
+    <div className="bg-red-100 min-h-screen">
+      {/* min h-screen */}
       {/* 새로 폭을 맞출 때는 min-h-screen을 쓴다 min-h-screen = height: 100vh; */}
       <header className="bg-blue-100 h-12 md:h-20">
         {" "}
@@ -24,7 +25,63 @@ function App() {
         <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
           여기에 나를 소개하는 내용을 적으면 됩니다.
         </div>
+        <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+          <li className="bg-purple-300 w-60 h-72 rounded-xl">
+            {/* object-fit: cover */}
+            <div className="bg-yellow-100 w-full h-1/2 rounded-t-xl">
+              프로젝트 이미지
+            </div>
+            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+          </li>
+        </ul>
+        <div className="bg-gray-100 max-w-screen-wl mx-auto h-96 flex justify-center items-center">
+          Joy's fortpolio
+        </div>
       </main>
+      <footer className="max-w-screen-xl mx-auto flex justify-end py-1 md:py-2">
+        2023, Designed by, Joy
+      </footer>
     </div>
   );
 }
